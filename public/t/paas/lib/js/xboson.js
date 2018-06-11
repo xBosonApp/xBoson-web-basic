aa939c2601df90f3a16c39d9f4c6c76fa790dc4f
yanming <yanming-sohu@sohu.com>
2018-06-11 17:49:41 +0800

    Signed-off-by: yanming <yanming-sohu@sohu.com>

--- public/t/paas/lib/js/xboson.js
+++ public/t/paas/lib/js/xboson.js
@@ -1,6 +1,14 @@
 (function($) {
   
 var nameCache = {};
+var ebus = {};
+
+//
+// 消息类型
+//
+var events = {
+  SELECT_TABLE_ROW: 'SELECT_TABLE_ROW',
+};
 
 
 var xb = window.xb = {
@@ -8,7 +16,53 @@ var xb = window.xb = {
   createDataTable     : createDataTable,
   select2fromApi      : select2fromApi,
   setupPagination     : setupPagination,
-};  
+  sendEvent           : sendEvent,
+  regListener         : regListener,
+  events              : events,
+  selectedTable       : selectedTable,
+  usebilityControl    : usebilityControl,
+};
+
+
+//
+// 向消息总线发送消息
+// type : 来自 events 中定义的常量.
+// id   : 发出组件的 ide
+// message: 消息参数
+//
+function sendEvent(type, id, message) {
+  console.debug("Send Event", type, id, message);
+  getEvent(type, id).fire(message);
+}
+
+
+//
+// 注册消息监听器
+//
+function regListener(type, id, callback) {
+  if (!callback) throw new Error("xboson.js:regListener callback is null");
+  getEvent(type, id).add(callback);
+  return {
+    remove: function() {
+      getEvent(type, id).remove(callback);
+    },
+  };
+}
+
+
+//
+// 内部函数, 构造消息总线
+//
+function getEvent(type, id) {
+  if (!type) throw new Error("xboson.js:getEvent type is null");
+  if (!id  ) throw new Error("xboson.js:getEvent id is null");
+  var n = type +":"+ id;
+  var v = ebus[n];
+  if (!v) {
+    v = ebus[n] = $.Callbacks();
+  }
+  return v;
+}
 
 
 //
@@ -59,7 +113,7 @@ function setupPagination(jobj, pageset, pageChangeCB) {
 // 绑定 form 查询条件表单, 从接口获取数据, 绑定分页.
 //
 function createDataTable(table) {
-  var id      = table.data('id');
+  var id      = table.attr('id');
   var form    = $(table.data('form'));
   var pageset = defaultPageset();
   var mapper  = table.find('.table_mapper');
@@ -72,6 +126,7 @@ function createDataTable(table) {
     "columns"   : getColumnMapper(mapper),
   });
   //sendDataRequest(); // call from setupPagination()
+  selectedTable(table, onselect);
   form.submit(sendDataRequest);
   
   setupPagination(pagination, pageset, function() {
@@ -108,7 +163,12 @@ function createDataTable(table) {
         delete options.aaData;
       }
       table.dataTable(options);
+      onselect(null);
     }
+  }
+  
+  function onselect(data) {
+    if (id) sendEvent(events.SELECT_TABLE_ROW, id, data);
   }
   
   function getColumns(rows) {
@@ -163,12 +223,67 @@ function createDataTable(table) {
 }
 
 
+//
+// 从 api 返回的数据组装为 selet2
+//
 function select2fromApi(jobj) {
   zy.g.am.app = jobj.data('app');
   zy.g.am.mod = jobj.data('mod');
   zy.net.get('api/'+ jobj.data('api'),function(msg){
     if(msg){
       jobj.zySelectCustomData('', false, { width:'100%' }, msg.result);  
+    }
+  });
+}
+
+
+//
+// 使表格的行可以选择 (并固定)
+//
+function selectedTable(jtable, selectcb) {
+  jtable.on('click', 'tr', function(e) {
+    // 当前选择行 index
+    var thiz = $(this);
+    if (thiz.find('th').is('th') || thiz.find('td').hasClass('dataTables_empty')) {
+      selectcb();
+      return false;
+    }
+    
+    // 变换选择行状态颜色
+    if (!thiz.find('div').hasClass('popover') && thiz.hasClass('active')) {
+      thiz.removeClass('active');
+      //$('#user_view_edit').btnDisable(true);
+    } else {
+      jtable.DataTable().$('tr.active').removeClass('active');
+      thiz.addClass('active');
+      //$('#user_view_edit').btnDisable(false);
+    }
+    selectcb(jtable.DataTable().row('.active').data(), thiz);
+  });
+}
+
+
+//
+// 控制组件 可用/不可用
+//
+function usebilityControl(accused) {
+  var thiz = $(accused);
+  var id   = thiz.attr('recvid');
+  var from = $('#'+id);
+  var ctrl = thiz.parent().add(thiz);
+  ctrl.prop('disabled', true);
+  
+  from.each(function() {
+    var tagname = this.tagName.toLowerCase();
+    var from = $(this);
+    
+    // 不同的控制组件发出不同的消息
+    switch (tagname) {
+      case 'table':
+        regListener(events.SELECT_TABLE_ROW, id, function(data) {
+          ctrl.prop('disabled', !data);
+        });
+        break;
     }
   });
 }
