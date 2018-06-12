f4d4b9803c8b3828f3095d0c43c2e9fd394ba572
yanming <yanming-sohu@sohu.com>
2018-06-12 21:49:18 +0800

    Signed-off-by: yanming <yanming-sohu@sohu.com>

---
+++ public/t/paas/datas/webservice/wsdl.js
@@ -0,0 +1,75 @@
+(function(data, type, id, handle) {
+  var wsdl = $('#wsdl2').find('.wsdl_define');
+  var func_def = $('#wsdl2').find('.function_define');
+  var func_tpl = $('#wsdl2').find('.function_define_tpl').html();
+  var currentJdom;
+  
+  wsdl.find(":input").prop('readonly', true);
+  currentJdom = wsdl;
+  setVal('[name=doc]', nohtml(data.wsdl.doc));
+  setSelect(wsdl.find('[name=types]'), data.wsdl.types);
+  setSelect(wsdl.find('[name=ns]'), data.wsdl.ns);
+  wsdl.removeClass('hide');
+  
+  func_def.empty().removeClass('hide');
+  for (var modname in data.wsdl.modules) {
+    var mod = data.wsdl.modules[modname];
+    var ctype = mod.ctype;
+    
+    for (var funcname in mod.functions) {
+      var func = mod.functions[funcname];
+      var funcdefdiv = $(func_tpl);
+      funcdefdiv.appendTo(func_def).find(":input").prop('readonly', true);
+      funcdefdiv.addClass('wsdl_function').addClass(ctype);
+      currentJdom = funcdefdiv;
+      setVal('.fname',        funcname);
+      setVal('[name=doc]',    nohtml(func.doc));
+      setVal('[name=curl]',   func.curl);
+      setVal('[name=mod]',    modname);
+      setVal('[name=ctype]',  ctype);
+      setVal('[name=input]',  JSON.stringify(func.input));
+      setVal('[name=output]', JSON.stringify(func.output));
+      
+      if (ctype == 'soap') {
+        openApply(funcdefdiv, func, modname);
+      }
+    }
+  }
+  
+  xb.sendEvent(xb.events.PAGE_UPDATED, null, wsdl);
+  
+  
+  function openApply(funcdefdiv, func, modname) {
+    var apply = funcdefdiv.find('[name="apply"]');
+    apply.removeClass('hide');
+    apply.click(function() {
+      $('a.tab_create').trigger('click');
+    });
+    currentJdom = $(".update_form");
+    setVal('[name=wsnote]',         nohtml(func.doc));
+    setVal('[name=ws_uri]',         func.curl);
+    setVal('[name=ws_mod_name]',    modname);
+    setVal('[name=ws_func_name]',   func.name);
+    setVal('[name=ws_config_json]', JSON.stringify(func));
+  }
+  
+  function setVal(selecter, val) {
+    var j = currentJdom.find(selecter);
+    if (j.is(':input')) {
+      j.val(val || '无');
+    } else {
+      j.html(val || '无');
+    }
+  }
+  
+  function setSelect(selecter, data) {
+    var sub = currentJdom.find(selecter);
+    for (var n in data) {
+      sub.append("<option>"+ n +'['+ data[n] + "]</option>");
+    }
+  }
+  
+  function nohtml(txt) {
+    return $("<p>"+ txt + "</p>").text();
+  }
+})
--- public/t/paas/lib/js/xboson.js
+++ public/t/paas/lib/js/xboson.js
@@ -7,7 +7,13 @@ var ebus = {};
 // 消息类型
 //
 var events = {
-  SELECT_TABLE_ROW: 'SELECT_TABLE_ROW',
+  // 表格行选择事件, 参数是当前行选择行的数据集, 
+  // 参数为 null 时说明没有选择任何行
+  SELECT_TABLE_ROW : 'SELECT_TABLE_ROW',
+  // 当 api 接口返回后的事件, 参数是接口的返回数据集.
+  FORM_API_RESULT  : 'FORM_API_RESULT',
+  // 画面重新绘制后发出这个时间, 不需要事件 id
+  PAGE_UPDATED     : 'PAGE_UPDATED',
 };
 
 
@@ -21,6 +27,16 @@ var xb = window.xb = {
   events              : events,
   selectedTable       : selectedTable,
   usebilityControl    : usebilityControl,
+  autoWidth           : autoWidth,
+  sendDataRequest     : sendDataRequest,
+  getText             : getText,
+  regListenerWithCode : regListenerWithCode,
+  toggleDisplay       : toggleDisplay,
+  postDataUseApi      : postDataUseApi,
+  sendApiResultEvent  : sendApiResultEvent,
+  valiStrMinMax       : valiStrMinMax,
+  valiStruct          : valiStruct,
+  vali_bind           : vali_bind,
 };
 
 
@@ -31,7 +47,7 @@ var xb = window.xb = {
 // message: 消息参数
 //
 function sendEvent(type, id, message) {
-  console.debug("Send Event", type, id, message);
+  zy.log("Send Event", type, id, message);
   getEvent(type, id).fire(message);
 }
 
@@ -42,6 +58,7 @@ function sendEvent(type, id, message) {
 function regListener(type, id, callback) {
   if (!callback) throw new Error("xboson.js:regListener callback is null");
   getEvent(type, id).add(callback);
+  zy.log("Reg Event Listener", type, id);
   return {
     remove: function() {
       getEvent(type, id).remove(callback);
@@ -54,6 +71,7 @@ function regListener(type, id, callback) {
 // 内部函数, 构造消息总线
 //
 function getEvent(type, id) {
+  if (type == events.PAGE_UPDATED) id = 'GLOBAL';
   if (!type) throw new Error("xboson.js:getEvent type is null");
   if (!id  ) throw new Error("xboson.js:getEvent id is null");
   var n = type +":"+ id;
@@ -110,6 +128,55 @@ function setupPagination(jobj, pageset, pageChangeCB) {
 
 
 //
+// 处理 get/post 返回结果, 弹出消息, 发送数据到消息总线
+//
+function sendApiResultEvent(ret, id) {
+  try {
+    if (ret.code == 0) {
+      zy.ui.msg("成功", ret.msg, 's');
+      xb.sendEvent(xb.events.FORM_API_RESULT, id, ret);
+    } else {
+      zy.ui.msg("失败", ret.msg, 'e');
+    }
+  } catch(err) {
+    zy.ui.msg("异常", err.message, 'e');
+    console.error(err);
+  }
+}
+
+
+//
+// Get 方法调用接口, jdata 中有 data[app, mod, api], jform 是表单参数,
+// pageset 可以空
+//
+function sendDataRequest(jdata, jform, pageset, reciveDataCB) {
+  if (pageset) {
+    zy.g.am.pagesize = pageset.pageSize;
+    zy.g.am.pagenum  = pageset.currentPage;
+  }
+  zy.g.am.app = jdata.data('app');
+  zy.g.am.mod = jdata.data('mod');
+  var api = 'api/'+ jdata.data('api');
+  var conditions = jform.serialize();
+  
+  zy.net.get(api, reciveDataCB, conditions, pageset && pageset.currentPage);
+}
+
+
+//
+// Get 方法调用接口, jdata 中有 data[app, mod, api], jform 是表单参数
+//
+function postDataUseApi(jdata, jform, reciveDataCB) {
+  zy.g.am.app = jdata.data('app');
+  zy.g.am.mod = jdata.data('mod');
+  var api = 'api/'+ jdata.data('api');
+  var conditions = jform.serialize();
+  
+  zy.net.post(api, reciveDataCB, conditions, 0, reciveDataCB, new Error());
+}
+
+
+//
 // 绑定 form 查询条件表单, 从接口获取数据, 绑定分页.
 //
 function createDataTable(table) {
@@ -125,24 +192,17 @@ function createDataTable(table) {
     "data"      : [],
     "columns"   : getColumnMapper(mapper),
   });
-  //sendDataRequest(); // call from setupPagination()
+  
   selectedTable(table, onselect);
-  form.submit(sendDataRequest);
+  form.submit(request);
   
   setupPagination(pagination, pageset, function() {
-    sendDataRequest();
+    request();
   });
   
   
-  function sendDataRequest() {
-    zy.g.am.app = table.data('app');
-    zy.g.am.mod = table.data('mod');
-    zy.g.am.pagesize = pageset.pageSize;
-    zy.g.am.pagenum  = pageset.currentPage;
-    var api = 'api/'+ table.data('api');
-    var conditions = form.serialize();
-    
-    zy.net.get(api, reciveData, conditions, pageset.currentPage);
+  function request() {
+    sendDataRequest(table, form, pageset, reciveData);
   }
   
   function reciveData(ret) {
@@ -287,5 +347,201 @@ function usebilityControl(accused) {
     }
   });
 }
+
+
+//
+// 依据 frame 子元素 col 的排列来返回宽度, 以更好的显示窗口.
+//
+function autoWidth(frame) {
+  var width = '';
+  if (frame.find('.col-1').length) {
+    width = '90%';
+  }
+  else if (frame.find('.col-2').length) {
+    width = '84%';
+  }
+  else if (frame.find('.col-3').length) {
+    width = '75%';
+  }
+  else if (frame.find('.col-4').length) {
+    width = '66%';
+  }
+  else if (frame.find('.col-5').length) {
+    width = '59%';
+  }
+  else if (frame.find('.col-6').length) {
+    width = '50%';
+  }
+  return width;
+}
+
+
+//
+// 获取文本.
+// url -- 以 '/' 开始则从 paas 下获取文件, 否则在 saas 中获取文件
+// callback -- Function(err, text);
+//
+function getText(url, callback) {
+  var _u = zy.g.host.ui + (zy.debug? '/t':'/ui');
+  if ('/' === url.charAt(0)) {
+    _u += '/paas/'+ url;
+  } else {
+    _u += '/saas/' + zy.g.comm.org + '/' + url;
+  }
+  
+  $.ajax({
+    type: 'get', url: _u, dataType: 'text',
+    success: function(txt) {
+      callback(null, txt);
+    },
+    error: function(xhr, ajax, error) {
+      callback(error);
+    },
+  });
+}
+
+
+//
+// 设置(消息总线)事件监听器,
+// code -- js 代码文本, 代码中必须返回一个函数
+// type -- 消息类型
+// pid  -- 消息 id
+//
+// 函数模板: (function(data, type, id, handle) { })
+//    data   : 接收发来的数据.
+//    type   : 消息类型
+//    id     : 消息 id
+//    handle : 标签本身的 jquery 对象
+//
+function regListenerWithCode(code, type, pid, handle) {
+  var fn = eval('('+ code + ')');
+  if (typeof fn != 'function') {
+    zy.ui.msg("错误 &lt;mp:handle>", "标签体中必须定义一个函数", 'e');
+    return;
+  }
+  
+  xb.regListener(type, pid, function(data) {
+    try {
+      fn(data, type, pid, handle);
+    } catch(e) {
+      zy.ui.msg("regListenerWithCode 处理器异常", e.stack, 'e');
+      throw e;
+    }
+  });
+}
+
+
+function toggleDisplay(controlor, accusedSelector, reverse) {
+  var refindcb = jQuery.Callbacks();
+  var accused;
+  var ret = {
+    setDisplay : setDisplay,
+    onrefind   : function(fn) { refindcb.add(fn) },
+  };
+  
+  refind();
+  regListener(events.PAGE_UPDATED, null, refind);
+  
+  function refind() {
+    accused = $(accusedSelector);
+    refindcb.fire();
+  }
+  
+  function setDisplay(disp_bool) {
+    if (reverse) {
+      if (!disp_bool) {
+        accused.show();
+      } else {
+        accused.hide();
+      }
+    } else {
+      if (disp_bool) {
+        accused.show();
+      } else {
+        accused.hide();
+      }
+    }
+  }
+  
+  return ret;
+}
+
+
+//
+// 构建 字符串 验证配置
+//
+function valiStrMinMax(struct) {
+  var fieldname = struct.unit.attr('name');
+  var tagname = struct.tagname;
+  var max  = struct.jdata.attr('max');
+  var min  = struct.jdata.attr('min');
+  
+  if (isNaN(min) || min < 0) {
+    zy.ui.msg(tagname +" 标签异常", 'min 值无效', 'w');
+  }
+  if (max && max < min) {
+    zy.ui.msg(tagname +" 标签异常", 'max 值无效', 'w');
+  }
+  
+  var rules = struct.setting.rules;
+  //
+  // 必须写成合并式的语法.
+  //
+  rules[fieldname] = $.extend(true, rules[fieldname], {
+    required  : min > 0,
+    minlength : parseInt(min),
+    maxlength : parseInt(max) || Number.MAX_VALUE,
+  });
+  vali_bind(struct);
+}
+
+
+//
+// 将配置与 form 绑定
+//
+function vali_bind(struct) {
+  var v = struct.form.validate(struct.setting);
+  // hack the validate settings.
+  $.extend(true, v.settings.rules, struct.setting.rules);
+}
+
+
+//
+// 构建 validate 结构, 返回验证数据结构对象.
+//
+function valiStruct(jdata, tagname) {
+  var form = jdata.parents("form").first();
+  var unit = jdata.parent();
+  
+  if (! unit.is(":input")) {
+    unit = unit.find(":input");
+  }
+  if (unit.length < 1) {
+    return zy.ui.msg(tagname +" 标签异常", '没有找到父级表单元件', 'e');
+  }
+  if (form.length < 1) {
+    return zy.ui.msg(tagname +" 标签异常", '没有找到表单', 'e');
+  }
+  
+  var setting = form.data("vali:data");
+  if (!setting) {
+    setting = { rules: {}, message: {} };
+    form.data("vali:data", setting);
+  }
+  
+  setting.errorPlacement = function(error, element) {  
+    error.insertAfter(element);
+    error.css({'position': 'absolute', 'right': '1px'});
+  };
+  
+  return {
+    unit    : unit,
+    form    : form,
+    setting : setting,
+    tagname : tagname,
+    jdata   : jdata,
+  };
+}
+
 
 })(jQuery);
