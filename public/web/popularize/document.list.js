c59ee0c37f5a48a01aa3d9e10da3368220bc8858
yanming <yanming-sohu@sohu.com>
2018-12-11 17:00:45 +0800

    增加: 宣传页的文档下载.
    Signed-off-by: yanming <yanming-sohu@sohu.com>

---
+++ public/web/popularize/document.list.js
@@ -0,0 +1,12 @@
+/* Create By xBoson System */
+//
+// 文件列表, 物理文件名不要用中文! 这些文件不受 git 管理.
+// 物理文件存储于 '/public/docx' 目录
+//
+var document_download_list = [
+  { file: 'xboson-basic-share-p.pdf', name: 'xBoson基础信息共享平台', date: '2018-12-11',
+    desc: '以基础信息标准化为核心，开放式共享服务为载体，为医疗卫生机构量身定制的解决方案' },
+    
+  { file: 'xboson-white-paper.pdf', name: 'xBoson区块链云平台白皮书', date: '2018-10-5',
+    desc: '基于场景，具备与现有系统实现快速整合能力，同时降低区块链技术应用难度' },
+];
