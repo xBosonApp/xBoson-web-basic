78af661298192d55dfdba13b69916aa347d7302e
yanming <yanming-sohu@sohu.com>
2018-12-26 11:32:56 +0800

    Signed-off-by: yanming <yanming-sohu@sohu.com>

---
+++ public/web/sites/it/document.list.js
@@ -0,0 +1,24 @@
+/* Create By xBoson System */
+//
+// 文件列表, 物理文件名不要用中文! 这些文件不受 git 管理.
+// 物理文件存储于 '/public/docx' 目录
+//
+var document_download_list = [
+  { file: 'xboson-basic-share-p.pdf', name: 'xBoson 基础信息共享平台', date: '2018-12-11',
+    desc: '以基础信息标准化为核心，开放式共享服务为载体，为医疗卫生机构量身定制的解决方案。' },
+
+  { file: 'health-internet-service.pdf', name: 'xBoson 医疗卫生互联网服务平台', date: '2018-12-11',
+    desc: '以服务为核心，互联网为载体，区块链为关键技术，为医疗卫生机构量身定制的解决方案。' },
+    
+  { file: 'xboson-white-paper.pdf', name: 'xBoson 区块链云平台白皮书', date: '2018-10-5',
+    desc: '基于场景，具备与现有系统实现快速整合能力，同时降低区块链技术应用难度。' },
+
+  { file: 'xboson-baas.pdf', name: 'xBoson 云服务开发平台', date: '2018-12-11',
+    desc: '基于微服务架构的BaaS开放式在线开发平台。' },
+
+  { file: 'xboson-esb.pdf', name: 'xBoson 数据服务总线ESB for Web', date: '2018-12-11',
+    desc: '轻量级企业服务总线（简称 ESB）是国内首款基于B/S架构，采用 NodeJS 技术实现，并完全拥有自主知识产权和核心技术，符合 SOA 架构标准的集成中间件产品。' },
+
+  { file: 'xboson-bigdata-paas.pdf', name: 'xBoson 大数据基础支撑平台', date: '2018-12-11',
+    desc: '大数据基础支撑平台是基于微服务架构的信息资源的一体化系统平台。主要包括大数据融合平台、信息标准管理平台、应用支撑平台等。【大数据融合平台】提供统一的数据采集、传输、存储、处理、管理、分析和可视化；【信息标准管理平台】提供主数据管理、数据元管理、数据集管理、标准服务等；【应用支撑平台】提供权限、统一身份认证登录、在线BaaS服务开发、服务授权管理、在线制图等应用支撑。' },
+];
