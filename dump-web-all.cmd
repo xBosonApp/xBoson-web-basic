2d266704d8a9b758d2ce36f07cf7038c82132bcb
yanming <yanming-sohu@sohu.com>
2019-09-16 11:11:48 +0800

    Signed-off-by: yanming <yanming-sohu@sohu.com>

--- dump-web-all.cmd
+++ dump-web-all.cmd
@@ -3,4 +3,5 @@ tar -czvpf web4xboson-all.tar.gz public shell-script webservice xboson-node-modu
 cp web4xboson-all.tar.gz g:\[project-backup]\xboson\xboson-web-dump-%DATE:~0,4%.%DATE:~5,2%.%DATE:~8,2%.tar.gz
 
 echo OK.
+echo tar -xzvf web4xboson-all.tar.gz
 pause
---
+++ dump-web-small.cmd
@@ -0,0 +1,4 @@
+tar --exclude ./public/*/saas --exclude ./public/web/his -czvpf web4xboson-small.tar.gz public shell-script webservice xboson-node-modules 
+
+echo OK.
+pause
