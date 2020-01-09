274a724d2435a0288aa2a059addbcae9b72f4f4d
yanming <yanming-sohu@sohu.com>
2020-01-09 11:13:58 +0800

    更新脚本

---
+++ move-docker-dir.cmd
@@ -0,0 +1,9 @@
+set target=D:\javaee-project\xboson-docker
+
+rem 移动 db/web/war 到 docker 目录中
+
+mv dump.db %target%\db\
+mv web4xboson*.tar.gz %target%\web\
+mv D:\javaee-project\xBoson\build\libs\xboson.war %target%\app\
+
+pause
