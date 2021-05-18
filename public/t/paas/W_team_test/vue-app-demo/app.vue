48456a31d4bf8c32538ff72fe9852734c0640ef1
yanming <yanming-sohu@sohu.com>
2021-05-18 21:19:01 +0800

    小型 vue app demo

---
+++ public/t/paas/W_team_test/vue-app-demo/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>{{ msg }} {{count}}</div>
+    <el-button v-on:click="count++">You clicked me {{ count }} times.</el-button> 
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello world!',
+      count: 0,
+    }
+  }
+}
+</script>
