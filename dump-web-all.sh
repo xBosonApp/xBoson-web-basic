769369bed7a1effe2606ac938d9ee2f04becc4e1
yanming <yanmingsohu@qq.com>
2021-07-10 17:32:43 +0800

    更新打包脚本

---
+++ dump-web-all.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+tar -czvpf web4xboson-all.tar.gz public shell-script webservice xboson-node-modules
---
+++ dump-web-small.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+tar -x web-small.filelist -czvpf web4xboson-all.tar.gz public shell-script webservice xboson-node-modules
